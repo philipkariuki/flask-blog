3308ae6a5c8603948c3a9578d6ec8b0bbaa8c3c0
philipkariuki <kariukiphil@gmail.com>
2019-07-08 08:44:48 +0300

    updates app/models.py defines Role class and creates roles table

--- app/models.py
+++ app/models.py
@@ -69,6 +69,14 @@ class User(UserMixin,db.Model):
         return users_emails
 
 
+class Role(db.Model):
+    __tablename__ = 'roles'
 
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return f'User {self.name}'
 
 
